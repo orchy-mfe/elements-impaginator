dbc2087469d5cafe6d3e3ac62a1eee96ca08b053
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-10-21 09:26:37 +0200

    Removed dropped content

--- src/components/page-content/PageContent.tsx
+++ src/components/page-content/PageContent.tsx
@@ -4,19 +4,17 @@ import ReactDOM from "react-dom";
 
 import {Configuration} from "../../models/Configuration";
 import {DroppableItem} from "../droppable-item/DroppableItem";
+import {observableConfiguration} from "../../stores/configuration";
 
 import Style from './PageContent.module.css'
-import {observableConfiguration} from "../../stores/configuration";
 
 export const PageContent = () => {
 
-    const [droppedContent, setDroppedContent] = useState<JSX.Element>()
     const [configuration, setConfiguration] = useState<Configuration>()
 
     const updateConfiguration = useCallback((configuration?: Configuration) => {
         if (configuration) {
             setConfiguration(configuration)
-            setDroppedContent(<DroppableItem configuration={configuration}/>)
         }
     }, [])
 
@@ -33,14 +31,14 @@ export const PageContent = () => {
             collect: monitor => ({
                 isOver: monitor.isOver({shallow: true})
             }),
-            canDrop: () => droppedContent === undefined,
+            canDrop: () => !configuration,
             drop: (item, monitor) => {
                 const droppedOnMe = !monitor.didDrop()
                 if (droppedOnMe) {
                     updateConfiguration(item)
                 }
             }
-        }), [droppedContent]
+        }), [configuration]
     )
 
     const onRefChange = useCallback((ref: any) => {
@@ -56,11 +54,11 @@ export const PageContent = () => {
         }
     }, [drop, configuration?.properties])
 
-    const background = {background: !droppedContent && isOver ? 'yellow' : 'white'}
+    const background = {background: !configuration && isOver ? 'yellow' : 'white'}
 
     return (
         <div ref={onRefChange} className={Style.pageContent} style={background}>
-            {droppedContent}
+            {configuration && <DroppableItem configuration={configuration}/>}
         </div>
     )
 }
--- src/models/ItemContextMenu.ts
+++ src/models/ItemContextMenu.ts
@@ -1,5 +1,17 @@
 import {MenuItem} from "primereact/menuitem";
 
+type BuildMenuItemsParams = {
+    deleteItem: () => void
+}
+
+type BuildMenuItemType = ({deleteItem}: BuildMenuItemsParams) => MenuItem[]
+
+export const buildMenuItems: BuildMenuItemType = ({deleteItem}: BuildMenuItemsParams) => [{
+    label: 'Delete',
+    icon: 'pi pi-minus-circle',
+    command: deleteItem,
+}]
+
 export const menuItems: MenuItem[] = [{
     label: 'Delete',
     icon: 'pi pi-minus-circle'
